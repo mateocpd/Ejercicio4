ebd37117d23a86196ee89ad34ecc6c25c21c859a
mateocpd <mateo.capdevila2801@gmail.com>
2023-06-06 20:17:58 -0300

    first commit

---
+++ Ejercicio 4.js
@@ -0,0 +1,6 @@
+const arregloOriginal = [23, 5, 34, 56, 98, "cadena", 7];
+console.log("Arreglo original:", arregloOriginal);
+
+
+const arregloInvertido = arregloOriginal.reverse();
+console.log("Arreglo invertido:", arregloInvertido);
